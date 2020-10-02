24c801088f25c4d5d064045d48c730ea9d99ead5
Michael Ryan <michael_ryan_2000@yahoo.com>
2020-10-02 17:25:28 -0400

    Separated eye and face detection

--- python/haarcascascades.py
+++ python/haarcascascades.py
@@ -15,11 +15,9 @@ while(True):
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     for (x,y,w,h) in faces:
         img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = img[y:y+h, x:x+w]
-        eyes = eye_cascade.detectMultiScale(roi_gray)
-        for (ex,ey,ew,eh) in eyes:
-            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+    eyes = eye_cascade.detectMultiScale(gray)
+    for (ex,ey,ew,eh) in eyes:
+        cv2.rectangle(img,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
     cv2.imshow('img', img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
